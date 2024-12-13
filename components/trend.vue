0ab26b0ad3556c1c82375d51c28e2dcbf5bea636
Samson DeVol <73560513+SamsonDeVol@users.noreply.github.com>
2024-12-12 18:35:40 -0800

    through step 67

--- components/trend.vue
+++ components/trend.vue
@@ -3,14 +3,14 @@
     <div class="font-bold" :class="[color]">{{title}}</div>
     <div class="text-2xl font-extrabold text-black dark:text-white mb-2">
       <USkeleton class="h-8 w-full" v-if="loading"/>
-      <div v-else>{{ amount }}</div>
+      <div v-else>{{ currency }}</div>
     </div>
     <div>
       <USkeleton class="h-6 w-full" v-if="loading" />
       <div v-else class="flex space-x-1 items-center text-sm">
         <UIcon :name="icon" class="w-6 h-6" :class="{'green': trendingUp, 'red': !trendingUp}"/>
         <div class="text-gray-500 dark:text-gray-400">
-          30% vs last period
+          {{ percentageTrend }} vs last period
         </div>
       </div>
     </div>
@@ -31,9 +31,17 @@ const trendingUp = computed(
 const icon = computed(
     () => trendingUp.value ? 'i-heroicons-arrow-trending-up' : 'i-heroicons-arrow-trending-down'
 )
+const {currency} = useCurrency(props.amount)
 const percentageTrend = computed(
     () => {
-      if(props.amount === 0 || props.lastAmount === 0) return '♠'
+      if(props.amount === 0 || props.lastAmount === 0) return 'inf'
+
+      const bigger = Math.max(props.amount, props.lastAmount)
+      const lower = Math.min(props.amount, props.lastAmount)
+
+      const ratio = ((bigger -lower) / lower) * 100
+      // console.log(bigger, lower, ratio, Math.ceil(ratio))
+      return `${Math.ceil(ratio)}%`
     }
 )
 </script>
--- pages/index.vue
+++ pages/index.vue
@@ -13,6 +13,13 @@
     <Trend color="green" title="Investments" :amount="4000" :last-amount="3000" :loading="false"/>
     <Trend color="red" title="Savings" :amount="4000" :last-amount="6000" :loading="false"/>
   </section>
+
+  <section>
+    <Transaction/>
+    <Transaction/>
+    <Transaction/>
+    <Transaction/>
+  </section>
 </template>
 
 <script setup>
